bdcf317b6bca05403cea6ac49a86f2c1d6582960
Kasper Sebb' brandt <sebb@sebb.dk>
2019-08-22 17:56:54 +0200

    Updates

---
+++ precache-manifest.d782d1a76253d9fd660fd856e8be0356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6bbb2dd33e7ee7c37fbec65acdcf2ae",
+    "url": "/vargr-okr/index.html"
+  },
+  {
+    "revision": "ecfd5346435f4a313f6c",
+    "url": "/vargr-okr/static/css/main.f9c3a882.chunk.css"
+  },
+  {
+    "revision": "0d642e8d3206c38b5e54",
+    "url": "/vargr-okr/static/js/2.840c3559.chunk.js"
+  },
+  {
+    "revision": "ecfd5346435f4a313f6c",
+    "url": "/vargr-okr/static/js/main.b9731ebf.chunk.js"
+  },
+  {
+    "revision": "aa829efc5fc5aebe0d0c",
+    "url": "/vargr-okr/static/js/runtime~main.08091c4a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vargr-okr/static/media/logo.5d5d9eef.svg"
+  }
+]);
